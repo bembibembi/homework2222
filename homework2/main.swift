985ee42aab5b99d3ce027f78f7a64ded7c557e2f
bembi <ismanovvvabembi123>
2022-10-21 18:17:25 +0600

    Initial Commit

---
+++ homework2/main.swift
@@ -0,0 +1,33 @@
+func R(R1: Int){
+    print(R1 * R1)
+}
+R(R1: 8)
+
+func H(H1: Int){
+    print(H1 * H1 * H1)
+}
+H(H1: 9)
+
+func BI(BI1: Int){
+    print(BI1 * 8)
+}
+BI(BI1: 9)
+
+func S (a:Int, b:Int) {
+    print((a+b) * 2 )
+}
+S(a: 6, b: 9)
+
+func K (a: Int, b: Double, s: Int ){
+    print(a * Int(b) * (s * s))
+}
+K(a: 2, b: 3.14, s: 2)
+
+
+    
+    
+
+
+
+
+
